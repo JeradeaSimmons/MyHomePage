2ba1ab056ffc3f654a83685dbc18862ae99a6f41
JeradeaSimmons <JeradeaSimmons@gmail.com>
2022-12-20 10:38:55 -0700

    getting it started

--- MyHomePage.client/src/App.vue
+++ MyHomePage.client/src/App.vue
@@ -2,11 +2,11 @@
   <header>
     <Navbar />
   </header>
-  <main>
+  <main class="container-fluid">
     <router-view />
   </main>
    <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
+    
   </footer>
 </template>
 
