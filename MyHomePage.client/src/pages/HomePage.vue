cfe60dcf9e5b5b07dc3b2996f4d89dde0007be67
JeradeaSimmons <JeradeaSimmons@gmail.com>
2023-01-02 10:49:00 -0700

    l

--- MyHomePage.client/src/pages/HomePage.vue
+++ MyHomePage.client/src/pages/HomePage.vue
@@ -14,7 +14,7 @@ links to
 -->
 <body>
 
-    <div class="row m-3">
+  <div class="row m-3">
       <div class="col-md-3">
         <div class="bg-white" style="text-align:center;padding:1em 0;">> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=large&timezone=America%2FBoise&show=hour_minute" width="100%" height="140" frameborder="0" seamless></iframe> </div>
       </div>
