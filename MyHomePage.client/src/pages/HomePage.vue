a1ed63b35fb95b96840f687be77798318ccb3c15
JeradeaSimmons <JeradeaSimmons@gmail.com>
2023-01-03 10:02:20 -0700

    still not what i'm wanting

--- MyHomePage.client/src/pages/HomePage.vue
+++ MyHomePage.client/src/pages/HomePage.vue
@@ -15,27 +15,23 @@ links to
 <body>
 
   <div class="row m-3">
-      <div class="col-md-3">
-        <div class="bg-white" style="text-align:center;padding:1em 0;">> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=large&timezone=America%2FBoise&show=hour_minute" width="100%" height="140" frameborder="0" seamless></iframe> </div>
-      </div>
-      <div class="col-md-5">
-        
-      </div>
+    <div class="col-md-3">
+      <div class="bg-white" style="text-align:center;padding:1em 0;">> <iframe
+          src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=large&timezone=America%2FBoise&show=hour_minute"
+          width="100%" height="140" frameborder="0" seamless></iframe> </div>
     </div>
-    <div class="row text-white text-center">
-      <iframe src="//widget.calendarlabs.com/v1/quot.php?cid=101&ver=1.2&uid=9442536361&c=random&l=en&cbg=FFFFFF&cb=1&cbc=000000&cf=calibri&cfg=000000&qfs=bi&qta=center&tfg=000000&tfs=bi&afc=000000&afs=i" frameborder="0"></iframe>
-   </div>
-    <div class="row text-white d-flex justify-content-evenly">
-      <div class="col-md-3">LOCAL/WORLD NEWS</div>
-      <div class="col-md-3">
-        <iframe width="900" height="1600" src="https://rss.app/embed/v1/wall/6Zz6HKydloq6WrqY" frameborder="0"></iframe>
-      </div>
-      <div class="col-md-3">
-        
-        
-      </div>
-      </div>
-    
+  </div>
+
+  <div class="row">
+    <iframe width="900" height="700" src="https://rss.app/embed/v1/imageboard/tIta2nNwsgtjjGup"
+      frameborder="0"></iframe>
+  </div>
+
+  <div class="row">
+    <iframe width="900" height="700" src="https://rss.app/embed/v1/imageboard/6Zz6HKydloq6WrqY"
+      frameborder="0"></iframe>
+  </div>
+
 </body>
 
 
